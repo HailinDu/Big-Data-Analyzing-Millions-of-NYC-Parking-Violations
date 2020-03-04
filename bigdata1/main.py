724210f7a1ba28ac2af634aeb320a2447e024ce4
Hailin Du <51971045+hailin-du@users.noreply.github.com>
2020-03-04 04:19:47 -0500

    Add files via upload

--- bigdata1/main.py
+++ bigdata1/main.py
@@ -1,12 +1,14 @@
 from sys import argv
-from src.bigdata1.opcvapi import call_opcv
+from src.bigdata1.callapi import call_api
 import os
+import argparse
 
 if __name__ == "__main__":
-	APP_KEY = os.getenv('APP_KEY')
-	page_size = int(argv[1])
-	num_pages = int(argv[2])
-	output = argv[3]
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--page_size', type = int)
+	parser.add_argument('--num_pages', default = None, type = int)
+	parser.add_argument('--output', default = None)
+	args = parser.parse_args()
 
-	data = call_opcv(APP_KEY, page_size, num_pages, output)
-	print(data)
+	APP_KEY = os.getenv('APP_KEY')
+	call_api(args.page_size, args.num_pages, args.output, APP_KEY)
